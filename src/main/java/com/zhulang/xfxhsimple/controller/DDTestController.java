51498b9176a2da2febfe8c82bd6870be9df20840
ddtxh <15307300010@163.com>
2023-11-03 17:33:23 +0800

    修复bug

--- src/main/java/com/zhulang/xfxhsimple/controller/DDTestController.java
+++ src/main/java/com/zhulang/xfxhsimple/controller/DDTestController.java
@@ -5,10 +5,8 @@ import com.zhulang.xfxhsimple.component.XfXhStreamClient;
 import com.zhulang.xfxhsimple.config.XfXhConfig;
 import com.zhulang.xfxhsimple.dto.MsgDTO;
 import com.zhulang.xfxhsimple.listener.XfXhWebSocketListener;
-import com.zhulang.xfxhsimple.utils.JedisPoolUtils;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.WebSocket;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +16,18 @@ import redis.clients.jedis.Jedis;
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.concurrent.FutureTask;
 
 @RestController
 @RequestMapping("/testone")
 @Slf4j
 public class DDTestController {
 
-    @Value("${server.synchronized}")
-    private String isSynchronized;
     @Resource
     private XfXhStreamClient xfXhStreamClient;
+
     @Resource
     private XfXhConfig xfXhConfig;
+
     @PostMapping("/flowtest")
     public Map<String, Object> flow(@RequestBody Map<String, Object> tokenMap) throws Exception {
         String asrAddr = "127.0.0.1:9988";
@@ -40,9 +37,7 @@ public class DDTestController {
         String callid = (String) tokenMap.get("callid");
         String appid = (String) tokenMap.get("appid");
         Map<String, Object> resultMap = new HashMap<>();
-        JedisPoolUtils jedisPoolUtils = new JedisPoolUtils();
-        Jedis jedis = jedisPoolUtils.getJedis();
-    if ("create".equals(method)) {
+        if ("create".equals(method)) {
             String callSource = (String) tokenMap.get("call_source");
             log.info("callSource ===>{}", callSource);
             String sourceName = (String) tokenMap.get("source_name");
@@ -80,33 +75,20 @@ public class DDTestController {
             //机器人没放音 0  在放音有时间
             Integer play_progress = (Integer) tokenMap.get("play_progress");
             log.info("play_progress ===>{}", play_progress);
+
+
             if ("complete".equals(input_type)) {
                 if (input_args.contains("hangup")) {
                     resultMap.put("action", "hangup");
                     resultMap.put("log", "挂机");
                 } else {
-                    if(null!=jedis.get("question")){
-                        //线程一
-                        ThreadTest mc = new ThreadTest(jedis.get("question"), xfXhStreamClient, xfXhConfig);
-                        // 4.创建一个FutureTask对象, 把MyCallable绑定到未来任务对象中
-                        FutureTask <String> task = new FutureTask<>(mc);
-                        // 5.把未来任务对象绑定到Thread类中
-                        Thread t = new Thread(task);
-                        t.start();
-                        //回答的问题转语音
-                        String str = task.get();
-                        resultMap.put("action", "cti_play_and_detect_speech");
-                        String date = LocalDateTime.now().toString();
-                        resultMap.put("argument", "'1' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '5000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
-                        resultMap.put("privatedata", "test");
-                        resultMap.put("playbacks", Collections.singletonList(str));
-                        resultMap.put("quickresponse", true);
-                        resultMap.put("log", "重新开始放音");
-                        //将缓存清空
-                        jedis.del("question");
-                    }else{
-
-                    }
+                    resultMap.put("action", "cti_play_and_detect_speech");
+                    String date = LocalDateTime.now().toString();
+                    resultMap.put("argument", "'1' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '5000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
+                    resultMap.put("privatedata", "test");
+                    resultMap.put("playbacks", Collections.singletonList("您可以继续向我提问"));
+                    resultMap.put("quickresponse", true);
+                    resultMap.put("log", "重新开始放音");
                 }
             } else {
                 String prefix = StrUtil.sub(input_args, 0, 1);
@@ -128,13 +110,12 @@ public class DDTestController {
                     }
                     else{
                         if(0<play_progress&&text.length()>3||0==play_progress) {
-                            jedis.set("question", text);
-                            System.err.println(jedis.get("question"));
+                            String str = sendQuestion(text);
                             resultMap.put("action", "cti_play_and_detect_speech");
                             String date = LocalDateTime.now().toString();
-                            resultMap.put("argument", "'1' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '5000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
+                            resultMap.put("argument", "'0' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '5000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
                             resultMap.put("privatedata", "test");
-                            resultMap.put("playbacks", Arrays.asList("查询中请稍后"));
+                            resultMap.put("playbacks", Arrays.asList(str));
                             resultMap.put("quickresponse", true);
                             resultMap.put("log", "播放识别结果");
                         }
@@ -143,7 +124,7 @@ public class DDTestController {
                 if ("D".equals(prefix)) {
                     resultMap.put("action", "cti_play_and_detect_speech");
                     String date = LocalDateTime.now().toString();
-                    resultMap.put("argument", "'1' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '10000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
+                    resultMap.put("argument", "'0' '1' '0' '0.8' '" + asrAddr + "' '120' '800' '10000' '20000' '' '' '" + appid + "' '1' '" + date + "' 'wav'");
                     resultMap.put("privatedata", "test");
                     resultMap.put("dtmf_terminators", "#");
                     resultMap.put("playbacks", Arrays.asList("刚刚的按键内容是", text, "请继续按键测试吧,并以#号键结束"));
@@ -159,7 +140,60 @@ public class DDTestController {
     }
 
 
+    private String sendQuestion( String question) {
+        String answer = "";
+        Map<String, Object> resultMap = new HashMap<>();
+        // 如果是无效字符串，则不对大模型进行请求
+        if (StrUtil.isBlank(question)) {
+            log.info("提问讯飞大模型 无效问题，请重新输入  ===>{}", question);
+            return  "无效问题，请重新输入";
+        }
+        // 获取连接令牌
+        if (!xfXhStreamClient.operateToken(XfXhStreamClient.GET_TOKEN_STATUS)) {
+            log.info("提问讯飞大模型 当前大模型连接数过多，请稍后再试  ===>{}", question);
+            return  "当前大模型连接数过多，请稍后再试";
+        }
 
+        // 创建消息对象
+        MsgDTO msgDTO = MsgDTO.createUserMsg(question);
+        // 创建监听器
+        XfXhWebSocketListener listener = new XfXhWebSocketListener();
+        // 发送问题给大模型，生成 websocket 连接
+        WebSocket webSocket = xfXhStreamClient.sendMsg(UUID.randomUUID().toString().substring(0, 10), Collections.singletonList(msgDTO), listener);
+        if (webSocket == null) {
+            // 归还令牌
+            xfXhStreamClient.operateToken(XfXhStreamClient.BACK_TOKEN_STATUS);
+            log.info("提问讯飞大模型 系统内部错误，请联系管理员  ===>{}", question);
+            answer = "系统内部错误，请联系管理员";
+        }
+        try {
+            int count = 0;
+            // 为了避免死循环，设置循环次数来定义超时时长
+            int maxCount = xfXhConfig.getMaxResponseTime() * 5;
+            while (count <= maxCount) {
+                Thread.sleep(200);
+                if (listener.isWsCloseFlag()) {
+                    break;
+                }
+                count++;
+            }
+            if (count > maxCount) {
+                log.info("大模型响应超时，请联系管理员  ===>{}", question);
+                return  "大模型响应超时，请联系管理员";
+            }
+            // 响应大模型的答案
+            return answer = listener.getAnswer().toString();
+        } catch (InterruptedException e) {
+            log.error("错误：" + e.getMessage());
+            log.info("提问讯飞大模型 系统内部错误，请联系管理员 ===>{}", e.getMessage());
+            return answer = "系统内部错误，请联系管理员";
+        } finally {
+            // 关闭 websocket 连接
+            webSocket.close(1000, "");
+            // 归还令牌
+            xfXhStreamClient.operateToken(XfXhStreamClient.BACK_TOKEN_STATUS);
+        }
+    }
 
 
 
